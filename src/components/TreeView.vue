08fb3f902a2a2905fe94a1611c67e6b3df1562ea
Angel <92003900+Ardocus@users.noreply.github.com>
2024-07-23 13:42:59 -0400

    Work In Progress

--- src/components/TreeView.vue
+++ src/components/TreeView.vue
@@ -9,31 +9,17 @@
   </template>
   
   <script setup>
-  import { ref } from 'vue'
-  
-  const nodes = ref({
-    GemmaForClassification: { name: 'GemmaForClassification' },
-    Model: { name: 'Model' },
-    GemmaModel: { name: 'GemmaModel' },
-    Score: { name: 'Score' },
-    Linear: { name: 'Linear' }
-  })
-  
-  const edges = ref({
-    edge1: { source: 'GemmaForClassification', target: 'Model' },
-    edge2: { source: 'Model', target: 'GemmaModel' },
-    edge3: { source: 'GemmaForClassification', target: 'Score' },
-    edge4: { source: 'Score', target: 'Linear' }
-  })
-  
-  const layouts = ref({
-    nodes: {
-      GemmaForClassification: { x: 0.0, y: 0.0 },
-      Model: { x: -100.0, y: 100.0 },
-      GemmaModel: { x: -200.0, y: 200.0 },
-      Score: { x: 100.0, y: 100.0 },
-      Linear: { x: 200.0, y: 200.0 }
-    }
+  import { onMounted, ref } from 'vue'
+  import gemmaData from '/src/assets/Gemma.json'
+
+  const nodes = ref({})
+  const edges = ref({})
+
+  onMounted(() => {
+    nodes.value = gemmaData.nodes
+    edges.value = gemmaData.edges
+    console.log(nodes.value)
+    console.log(edges.value)
   })
   </script>
   
